8011420522d449dc1719d67552b03f9d676f2c96
yotaarai <yotaarai85@gmail.com>
2020-06-04 22:45:10 +0900

    fix: ホーム画面 includeを定義

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
+@section('title')
+<title>{{ config('app.name') }}
+</title>
+@endsection
+
+@include('layouts.favicon')
+
+@include('layouts.header')
+
+@include('layouts.message')
+
 @section('content')
 <div>
   <h2>ホーム画面</h2>
 </div>
 @endsection
+
+@include('layouts.footer')
